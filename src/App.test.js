92e424f41418b0ef96cd3249d9d81be1b4c9b169
Ijeoma Oforeh <ijeomaoforeh@Ijeomas-Air.cg.shawcable.net>
2022-10-30 18:34:26 -0600

    changed file to f

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn reacts/i);
+  const linkElement = screen.getByText(/learn reactf/i);
   expect(linkElement).toBeInTheDocument();
 });
